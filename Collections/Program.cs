8728013606aae32155ef1d308daaa5be22540597
Alyson Lima Ramalho <alysonlima.alr@gmail.com>
2022-01-16 13:07:01 -0300

    Trabalhando com coleções especificas: Stack

--- Collections/Program.cs
+++ Collections/Program.cs
@@ -1,12 +1,12 @@
 ﻿using Collections.Helper;
-Queue<string> fila = new Queue<string>();
-fila.Enqueue("Mariana Santana");
-fila.Enqueue("João Rodrigues");
-fila.Enqueue("Byanca Molero");
-System.Console.WriteLine($"Pessoas na fila: {fila.Count}");
-while (fila.Count > 0)
+Stack<string> pilhaLivros = new Stack<string>();
+pilhaLivros.Push("Universo Numa Casca De Nós");
+pilhaLivros.Push("Teoria da Relatividade");
+pilhaLivros.Push("Breves Respostas Para Grandes Questões");
+System.Console.WriteLine($"Quantidade de livros: {pilhaLivros.Count}");
+while (pilhaLivros.Count > 0)
 {
-    System.Console.WriteLine($"Vez de {fila.Peek()}");
-    System.Console.WriteLine($"chamando {fila.Dequeue()}");
-    System.Console.WriteLine(fila.Count);
-} 
+     System.Console.WriteLine($"Proximo livro para leitura: {pilhaLivros.Peek()}");
+     System.Console.WriteLine($"{pilhaLivros.Pop()} lido com sucesso!");
+     System.Console.WriteLine($"Livros restantes: {pilhaLivros.Count}");
+}
