a3334ad020e77d74e2e1204e49659ff535b13aed
Alyson Lima Ramalho <alysonlima.alr@gmail.com>
2022-01-16 00:09:36 -0300

    trabalhando com coleções genéricas (Listas)

--- Collections/Program.cs
+++ Collections/Program.cs
@@ -1,41 +1,7 @@
 ﻿using Collections.Helper;
-ArrayOperations operations = new ArrayOperations();
-int[] n = new int[5]{4,9,3,2,7};
-System.Console.WriteLine(n.GetType());
-string[] s = Array.ConvertAll(n, element=>element.ToString());
-
-System.Console.WriteLine(s.GetType());
-
-// operations.VerificAllLarger(n,10);
-// operations.VerificAllSmaler(n,1);
-// System.Console.WriteLine("----------------------------");
-// operations.VerificAllLarger(n,1);
-// operations.VerificAllSmaler(n,10);
-
-// operations.VerificExist(n,9);
-
-// Array.Sort(n);
-// int[] n1 = new int[10];
-// Array.Copy(n,n1,n.Length);
-// operations.PrintArray(n1);
-
-// System.Console.WriteLine("Array Original:");
-// operations.PrintArray(n);
-// Array.Sort(n);
-// System.Console.WriteLine("Array ordenado:");
-// operations.PrintArray(n);
-
-// int[,] matriz = new int[4, 2]{{8,8},{9,2},{3,4},{5,1}};
-// for (int i = 0; i < matriz.GetLength(0); i++)
-// {
-//     for (int j = 0; j < matriz.GetLength(1); j++)
-//     {
-//         System.Console.WriteLine($"matriz posição {i}, {j}: {matriz[i,j]}");
-//     }
-// }
-
-// int[] arrayInts = new int[3];
-// arrayInts[0]=10;
-// arrayInts[1]=20;
-// arrayInts[2]=30;
-// foreach(int i in arrayInts){System.Console.WriteLine(i);}
+List<string> states = new List<string>();
+states.Add("SP");
+states.Add("MG");
+states.Add("BA");
+System.Console.WriteLine($"Quantidade de elementos na lista: {states.Count}");
+foreach (var item in states){System.Console.WriteLine(item);}
