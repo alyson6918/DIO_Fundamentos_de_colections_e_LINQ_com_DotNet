500963c8eddfe7beca6794925faada20b028969b
Alyson Lima Ramalho <alysonlima.alr@gmail.com>
2022-01-15 23:05:51 -0300

    copiando um array com a classe Array

--- Collections/Program.cs
+++ Collections/Program.cs
@@ -1,11 +1,16 @@
 ﻿using Collections.Helper;
 ArrayOperations operations = new ArrayOperations();
 int[] n = new int[5]{4,9,3,2,1};
-System.Console.WriteLine("Array Original:");
-operations.PrintArray(n);
 Array.Sort(n);
-System.Console.WriteLine("Array ordenado:");
-operations.PrintArray(n);
+int[] n1 = new int[10];
+Array.Copy(n,n1,n.Length);
+operations.PrintArray(n1);
+
+// System.Console.WriteLine("Array Original:");
+// operations.PrintArray(n);
+// Array.Sort(n);
+// System.Console.WriteLine("Array ordenado:");
+// operations.PrintArray(n);
 
 // int[,] matriz = new int[4, 2]{{8,8},{9,2},{3,4},{5,1}};
 // for (int i = 0; i < matriz.GetLength(0); i++)
