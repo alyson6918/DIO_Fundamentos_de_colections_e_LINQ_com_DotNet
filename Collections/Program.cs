5428cd99ad03a523a5a65cf72aec2c9272643517
Alyson Lima Ramalho <alysonlima.alr@gmail.com>
2022-01-16 14:07:20 -0300

    trabalhando com LINQ: JOIN, Distinct e Sum

--- Collections/Program.cs
+++ Collections/Program.cs
@@ -1,3 +1,4 @@
 ﻿using Collections.Helper;
-int[] arrayNumbers = new int[7]{1,4,6,7,3,100,47};
-System.Console.WriteLine($"valor minimo: {arrayNumbers.Min()}\nValor médio: {arrayNumbers.Average()}\nValor maximo: {arrayNumbers.Max()}");
+int[] arrayNumbers = new int[10]{1,4,6,7,3,100,47,47,100,6};
+var arrayUnico = arrayNumbers.Distinct().ToArray();
+System.Console.WriteLine($"Soma de todos os valores do array: {arrayNumbers.Sum()}\nArray Original: {string.Join(", ",arrayNumbers)}\nArray distinto:{string.Join(", ",arrayUnico)}");
