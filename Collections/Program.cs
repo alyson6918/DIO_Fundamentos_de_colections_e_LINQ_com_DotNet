4a0f6fd06f40e00e99d2553582b21c225be66b4c
Alyson Lima Ramalho <alysonlima.alr@gmail.com>
2022-01-16 12:54:47 -0300

    coleções especificas Queue

--- Collections/Program.cs
+++ Collections/Program.cs
@@ -1,11 +1,12 @@
 ﻿using Collections.Helper;
-List<string> states = new List<string>{"SP", "MG", "BA"};
-string[] s1 = new string[2]{"SC","MT"};
-ListOperator op = new ListOperator();
-
-op.PrintList(states);
-System.Console.WriteLine("----------");
-states.Insert(1, "RJ");
-states.Remove("BA");
-states.AddRange(s1);
-op.PrintList(states);
+Queue<string> fila = new Queue<string>();
+fila.Enqueue("Mariana Santana");
+fila.Enqueue("João Rodrigues");
+fila.Enqueue("Byanca Molero");
+System.Console.WriteLine($"Pessoas na fila: {fila.Count}");
+while (fila.Count > 0)
+{
+    System.Console.WriteLine($"Vez de {fila.Peek()}");
+    System.Console.WriteLine($"chamando {fila.Dequeue()}");
+    System.Console.WriteLine(fila.Count);
+} 
